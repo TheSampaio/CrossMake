ea206d5532c4daa8e4bf8b122adb7866e3759259
Kellvyn Sampaio <64695440+TheSampaio@users.noreply.github.com>
2025-11-24 11:27:52 -0300

    Add run_setup.bat script to the project

---
+++ scripts/run_setup.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: Get the current folder name as project name
+FOR %%F IN (.) DO SET "PROJECT_NAME=%%~nF"
+
+:: Delete the existing README.md
+IF EXIST README.md DEL README.md
+
+:: Create a new README.md
+(
+ECHO # %PROJECT_NAME%
+ECHO.
+ECHO This project is a cross-platform C++ project initialized from the [CrossMake](https://github.com/TheSampaio/CrossMake^) template.
+ECHO.
+ECHO ## 📜 License
+ECHO This project template is released under the **MIT License**.
+ECHO.
+ECHO ## ⭐ Support
+ECHO If you find this template useful, consider starring the [CrossMake](https://github.com/TheSampaio/CrossMake^) repository on GitHub to help others discover it.
+) >> README.md
+
+:: Update CMakeLists.txt with the current project name
+IF EXIST CMakeLists.txt (
+    POWERSHELL -Command "(Get-Content CMakeLists.txt) -replace 'project\\(.* LANGUAGES CXX\\)', 'project(\"%PROJECT_NAME%\" LANGUAGES CXX)' | Set-Content CMakeLists.txt"
+)
+
+:: Delete .gitkeep files in include, tests, and vendor folders
+IF EXIST include\.gitkeep DEL include\.gitkeep
+IF EXIST tests\.gitkeep DEL tests\.gitkeep
+IF EXIST vendor\.gitkeep DEL vendor\.gitkeep
+
+ECHO Setup completed.
